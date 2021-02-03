5ebf6354113eb2d3e514528fd6f1dcd1a35bf808
Branden <branden.degrasse@uleth.ca>
2021-02-02 20:36:59 -0700

    added changing jokes and backgrounds

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,42 @@
-console.log("hello");
+$joke = $("#joke");
+$counter = "0";
 
-$(".joke").text("lol");
+console.log($counter);
 
-var $one = $("#l1");
-var $two = $("#l2");
+$joke.on("click", function(){
+	$counter++;
+	console.log($counter);
 
-$one.on("click", function(){
-	$(this).hide();
-	$two.show();
+	 if ($counter == "1"){
+ 	$joke.text("Why do potatoes make good detectives?");
+ 	$("body").css("background-color","pink");
+ }
+ 	else if ($counter == "2"){
+ 	$joke.text("Because they keep their eyes peeled.");
+ 	$("body").css("background-color","#B11D1D");
+ }
+ 	else if ($counter == "3"){
+ 	$joke.text("What do you get if you divide the circumference of a pumpkin by its diameter?");
+ 	$("body").css("background-color","#6EC907");
+ }
+
+	else if ($counter == "4"){
+ 	$joke.text("Pumpkin pi");
+ 	$("body").css("background-color","#0C790F");
+ }
+
+	else if ($counter == "5"){
+ 	$joke.text("Everyone has these on their face?");
+ 	$("body").css("background-color","#238EE1");
+ }
+
+	else if ($counter == "6"){
+ 	$joke.text("Tulips");
+ 	$("body").css("background-color","#7E189F");
+ }
+ 	else if($counter > "6"){
+ 	$joke.text("Reset the page to start again!");
+ 	$("body").css("background-color","#961C32");
+ };
 });
 
-$two.on("click", function(){
-	$(this).hide();
-	$one.show();
-});
